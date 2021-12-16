cf8927ceffc12d180f4d9a69ba426f31ce2bd6c1
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:14:35 +0100

    feature(catalog): models

---
+++ FranklyFashion.CatalogService/Models/DTOs/ProductCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FranklyFashion.CatalogService.Models;
+public class ProductCreateDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public int Price { get; set; }
+}
---
+++ FranklyFashion.CatalogService/Models/DTOs/ProductCreatedDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FranklyFashion.CatalogService.Models;
+public class ProductCreatedDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public int Price { get; set; }
+    public string UrlSlug { get; set; }
+}
