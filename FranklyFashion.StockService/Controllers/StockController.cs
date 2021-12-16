6fa33b8da27c61d07df626b83bea0f155f3ae7ef
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:15:10 +0100

    feature(stock): controller

---
+++ FranklyFashion.StockService/Controllers/StockController.cs
@@ -0,0 +1,47 @@
+﻿namespace FranklyFashion.StockService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    private readonly StockContext _context;
+    private readonly IMapper _mapper;
+    public StockController(StockContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPut("{articleNumber}")]
+    public async Task<IActionResult> UpdateStockLevel(string articleNumber, StockLevelPutDTO stockLevelDTO)
+    {
+        var stockLevel = await _context.StockLevels
+            .FirstOrDefaultAsync(s => s.ArticleNumber == articleNumber);
+
+        if (stockLevel == null)
+            await _context.AddAsync(
+                 _mapper.Map<StockLevel>(stockLevelDTO)
+                 );
+        else
+            stockLevel.Stock = stockLevelDTO.Stock;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<StockLevelListDTO>> GetAll()
+    {
+        var stockLevels = _context.StockLevels
+            .Select(s => _mapper.Map<StockLevelListDTO>(s));
+
+        if (stockLevels.Any())
+            return Ok(stockLevels);
+
+        return NoContent();
+    }
+
+
+}
+
