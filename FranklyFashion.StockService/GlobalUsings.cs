05b859bdb3b75085b907db84260029d333b57ef1
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:12:13 +0100

    chore(stockservice): global usings

---
+++ FranklyFashion.StockService/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using AutoMapper;
+global using FranklyFashion.StockService;
+global using FranklyFashion.StockService.Data;
+global using FranklyFashion.StockService.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
