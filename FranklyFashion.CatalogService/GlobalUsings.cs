4cc52d9112bd4e1893518bc393744e1f43abdf15
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:12:48 +0100

    chore(catalogservice): global usings

---
+++ FranklyFashion.CatalogService/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using AutoMapper;
+global using FranklyFashion.CatalogService.Data;
+global using FranklyFashion.CatalogService.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using System.Text.RegularExpressions;
