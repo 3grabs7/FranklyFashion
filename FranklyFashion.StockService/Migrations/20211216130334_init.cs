dbd400e9a73447836ba70462892fd2bee03ca78b
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:15:46 +0100

    feature(stock): migrations

---
+++ FranklyFashion.StockService/Migrations/20211216130334_init.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FranklyFashion.StockService.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockLevels",
+                columns: table => new
+                {
+                    ArticleNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockLevels", x => x.ArticleNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockLevels");
+        }
+    }
+}
