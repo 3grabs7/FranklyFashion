540b0a3b4f9c292f7488a3b7e051f4a86efb095c
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:16:29 +0100

    feature(catalog): db context

---
+++ FranklyFashion.CatalogService/Data/CatalogContext.cs
@@ -0,0 +1,6 @@
+﻿namespace FranklyFashion.CatalogService.Data;
+public class CatalogContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public CatalogContext(DbContextOptions options) : base(options) { }
+}
