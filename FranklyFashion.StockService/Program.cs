40803dffa6c25cd13dc37e377e60c7433d334957
Robin Grabs <robinmgrabs@gmail.com>
2021-12-16 19:18:06 +0100

    feature(stock): startup dbcontext automapper services

---
+++ FranklyFashion.StockService/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StockContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
+
+builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
