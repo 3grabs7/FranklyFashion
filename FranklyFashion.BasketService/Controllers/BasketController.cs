9f63290b00bf3a8c8f049a5766c339967a78111f
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:15:57 +0100

    feature(basket): controller

---
+++ FranklyFashion.BasketService/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿namespace FranklyFashion.BasketService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IDistributedCache _cache;
+    public BasketController(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    [HttpGet("{basketId}")]
+    public async Task<ActionResult<BasketDTO>> GetBasket(string basketId)
+    {
+        var basket = await _cache.GetStringAsync(basketId);
+        if (basket == null)
+            return NotFound();
+
+        var result = JsonSerializer.Deserialize<BasketDTO>(basket);
+
+        return Ok(result);
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<BasketDTO>> UpdateBasket(BasketDTO basket)
+    {
+        await _cache.SetStringAsync(basket.Identifier, JsonSerializer.Serialize(basket));
+        return await GetBasket(basket.Identifier);
+    }
+
+}
