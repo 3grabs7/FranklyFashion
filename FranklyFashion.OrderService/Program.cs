89ee4f035725e683f6be8250f458f10239227e9d
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:14:11 +0100

    feature(order): service registrations

--- FranklyFashion.OrderService/Program.cs
+++ FranklyFashion.OrderService/Program.cs
@@ -1,15 +1,21 @@
+using FranklyFashion.OrderService;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MapperProfile));
+builder.Services.AddHttpClient();
+
+builder.Services.AddDbContext<OrderContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("OrderService")
+        )
+    );
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
