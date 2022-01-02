fffb15d60cfa8f11d0d86aed2a8054901bbf8a74
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:17:17 +0100

    feature(basket): service registrations, cors allow any

--- FranklyFashion.BasketService/Program.cs
+++ FranklyFashion.BasketService/Program.cs
@@ -1,15 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+builder.Services.AddMemoryCache();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors(policy => policy.AllowAnyOrigin());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
