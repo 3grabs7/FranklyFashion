d6fc3915f90e66cc98f0407112e4e76f8f8a7072
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:13:09 +0100

    feature(order): order context

---
+++ FranklyFashion.OrderService/Data/OrderContext.cs
@@ -0,0 +1,9 @@
+﻿namespace FranklyFashion.OrderService.Data;
+
+public class OrderContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderLine> OrderLines { get; set; }
+
+    public OrderContext(DbContextOptions options) : base(options) { }
+}
