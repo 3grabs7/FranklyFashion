2ce99fa6ed4f28965a6da410dac75a40dd20827a
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:13:25 +0100

    feature(order): domain models

---
+++ FranklyFashion.OrderService/Models/Domain/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace FranklyFashion.OrderService.Models.Domain;
+public class Order
+{
+    public int Id { get; set; }
+    public string Customer { get; set; }
+    public ICollection<OrderLine> OrderLines { get; set; }
+}
