42b8ff7f9df0985b4e5d9838373b20abadb19071
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:13:53 +0100

    feature(order): mapper profile

---
+++ FranklyFashion.OrderService/MapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using FranklyFashion.OrderService.Models;
+
+namespace FranklyFashion.OrderService;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<BasketDTO, Order>()
+            .ForMember(dest => dest.OrderLines, opt => opt.MapFrom(src => src.BasketItems));
+        CreateMap<BasketItemDTO, OrderLine>();
+    }
+}
