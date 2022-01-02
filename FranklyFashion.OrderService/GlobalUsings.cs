9fe27e852f5ff68e8866cf81055dbe8e5075c751
Robin Grabs <robinmgrabs@gmail.com>
2022-01-02 14:15:23 +0100

    chore(order): global usings

---
+++ FranklyFashion.OrderService/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using AutoMapper;
+global using FranklyFashion.OrderService.Data;
+global using FranklyFashion.OrderService.Models.Domain;
+global using FranklyFashion.OrderService.Models.DTOs;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+
+
